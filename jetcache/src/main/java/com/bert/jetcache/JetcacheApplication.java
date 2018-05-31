9d953fc8fb89992b7c56b184b2a4651f187f2950
yangbo <bo.yang@lvyuetravel.com>
2018-05-31 21:28:42 +0800

    add

---
+++ jetcache/src/main/java/com/bert/jetcache/JetcacheApplication.java
@@ -0,0 +1,13 @@
+package com.bert.jetcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetcacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetcacheApplication.class, args);
+    }
+
+}
