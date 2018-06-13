0d5466ef4fca137d2efc00613d67a84d1d298e73
yangbo <bo.yang@lvyuetravel.com>
2018-06-13 11:00:37 +0800

    add restTemplate support

--- swagger/src/main/java/com/bert/swagger/config/Swagger2.java
+++ swagger/src/main/java/com/bert/swagger/config/Swagger2.java
@@ -1,7 +1,20 @@
 package com.bert.swagger.config;
 
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -10,6 +23,10 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.net.ssl.SSLContext;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 /**
  * @author yangbo
@@ -37,6 +54,38 @@ public class Swagger2 {
                 .contact(new Contact("Bert", "https://www.baidu.com/", "330719895@qq.com"))
                 .version("1.0")
                 .build();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() throws Exception {
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        //setup a Trust Strategy that allows all certificates.
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+            @Override
+            public boolean isTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                return true;
+            }
+        }).build();
+        httpClientBuilder.setSSLContext(sslContext);
+        /*
+         * don't check Hostnames, either.
+         * use SSLConnectionSocketFactory.getDefaultHostnameVerifier(), if you don't want to weaken
+         */
+        SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", sslSocketFactory)
+                .build();
+        //now, we create connection-manager using our Registry. allows multi-threaded use
+        PoolingHttpClientConnectionManager connMgr = new PoolingHttpClientConnectionManager(socketFactoryRegistry);
+        connMgr.setMaxTotal(200);
+        connMgr.setDefaultMaxPerRoute(100);
+        httpClientBuilder.setConnectionManager(connMgr);
+
+        CloseableHttpClient httpClient = httpClientBuilder.build();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+        return restTemplate;
     }
 
 }
--- swagger/src/main/java/com/bert/swagger/controller/TestController.java
+++ swagger/src/main/java/com/bert/swagger/controller/TestController.java
@@ -4,10 +4,20 @@ import com.bert.swagger.common.Message;
 import com.bert.swagger.model.User;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * @author yangbo
@@ -15,6 +25,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+    @Resource
+    private RestTemplate restTemplate;
 
     @ApiOperation(value = "测试方法1", notes = "我的第一个Swagger2方法")
     @GetMapping("test1")
@@ -27,6 +40,16 @@ public class TestController {
     @PostMapping("test2")
     public Message test2(@RequestBody User user) {
         return Message.success(user);
+    }
+
+    @RequestMapping("test3")
+    public Message test3() {
+        String url = "https://services.expediapartnercentral.com/products/properties";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encodeBase64String("EQCtest12933870:ew67nk33".getBytes()));
+        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<>(null, httpHeaders);
+        ResponseEntity<String> object = restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+        return Message.success(object);
     }
 
 }
