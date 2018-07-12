d394917890de856a1d631ad708f44bb58ad824c1
yangbo <bo.yang@lvyuetravel.com>
2018-07-12 15:28:06 +0800

    update

--- config-client/src/main/java/com/bert/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/bert/spring/cloud/config/ConfigClientApplication.java
@@ -1,13 +1,15 @@
 package com.bert.spring.cloud.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@Slf4j
 @RestController
+@SpringBootApplication
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
@@ -15,10 +17,15 @@ public class ConfigClientApplication {
     }
 
     @Value("${foo}")
-    String foo;
+    private String foo;
+
+    @Value("${foo1}")
+    private String foo1;
 
     @RequestMapping(value = "/hi")
-    public String hi(){
+    public String hi() {
+        log.info("###foo:{}", foo);
+        log.info("###foo1:{}", foo1);
         return foo;
     }
 
