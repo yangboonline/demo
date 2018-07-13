c986ea338fff189e63cabf9b47bd447b7fac3938
yangbo <bo.yang@lvyuetravel.com>
2018-07-13 14:25:07 +0800

    update

--- config-client/src/main/java/com/bert/spring/cloud/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/bert/spring/cloud/config/ConfigClientApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
