bf2994ceccbc80384df10710257c8edbb2f397b7
yangbo <bo.yang@lvyuetravel.com>
2018-07-16 19:07:16 +0800

    update

---
+++ eureka-client/src/main/java/com/bert/spring/cloud/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,23 @@
+package com.bert.spring.cloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
+
+}
