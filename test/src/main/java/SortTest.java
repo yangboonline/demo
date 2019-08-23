218e13a8c9b250e2fa3491a5841af01ef1375d2b
yangbo <bo.yang@lvyuetravel.com>
2019-08-23 15:20:08 +0800

    update

--- test/src/main/java/SortTest.java
+++ test/src/main/java/SortTest.java
@@ -6,8 +6,49 @@ public class SortTest {
         int[] arr = {21, 23, 4, 5, 2, 5, 3, 9, 0, 1, 2};
         Arrays.stream(arr).forEach(t -> System.out.print(t + " "));
         System.out.println();
-        bubbleSort(arr);
+        //bubbleSort(arr);
+        quickSort(arr, 0, arr.length - 1);
         Arrays.stream(arr).forEach(t -> System.out.print(t + " "));
+    }
+
+    /**
+     * 快排 quickSort(arr, 0, arr.length - 1);
+     */
+    private static void quickSort(int[] arr, int left, int right) {
+        int l = left;
+        int r = right;
+        int pivod = arr[(left + right) / 2];
+        int temp;
+        while (l < r) {
+            while (arr[l] < pivod) {
+                l++;
+            }
+            while (arr[r] > pivod) {
+                r--;
+            }
+            if (l >= r) {
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+            if (arr[l] == pivod) {
+                r--;
+            }
+            if (arr[r] == pivod) {
+                l--;
+            }
+        }
+        if (l == r) {
+            l++;
+            r--;
+        }
+        if (left < r) {
+            quickSort(arr, left, r);
+        }
+        if (right > l) {
+            quickSort(arr, l, right);
+        }
     }
 
     /**
