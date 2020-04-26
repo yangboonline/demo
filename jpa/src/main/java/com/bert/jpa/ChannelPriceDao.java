b720b22b5737b29acfde12c50be58f1754ec4ef9
yangbo <boyang217740@sohu-inc.com>
2020-04-26 18:10:42 +0800

    update

--- jpa/src/main/java/com/bert/jpa/ChannelPriceDao.java
+++ jpa/src/main/java/com/bert/jpa/ChannelPriceDao.java
@@ -1,5 +1,6 @@
 package com.bert.jpa;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -13,11 +14,38 @@ import java.util.List;
  */
 public interface ChannelPriceDao extends JpaRepository<ChannelPriceEntity, Long>, JpaSpecificationExecutor<ChannelPriceEntity> {
 
+    /**
+     * 原生查询
+     */
     @Query(nativeQuery = true,
             value = "SELECT * FROM t_switch_channel_stock WHERE 1 = 1 "
-                    + "AND IFNULL(id IN (:ids), 1 = 1) "
-                    + "AND IFNULL(deleted = :deleted, 1 = 1) ")
+                    + "AND (:ids IS NULL OR id IN (:ids)) "
+                    + "AND (:deleted IS NULL OR deleted = :deleted) ")
     List<ChannelPriceEntity> findAllByIdInAndDeletedNative(@Param("ids") List<Long> ids,
                                                            @Param("deleted") Integer deleted);
+
+    /**
+     * 投影查询
+     */
+    @Query(nativeQuery = true,
+            value = "SELECT id, pms_sys_code AS pmsSysCode,access_platform FROM t_switch_channel_stock WHERE 1 = 1 "
+                    + "AND (:ids IS NULL OR id IN (:ids)) "
+                    + "AND (:deleted IS NULL OR deleted = :deleted) ")
+    List<ChannelPriceEntityProjection> findAllByIdInAndDeletedProjection(@Param("ids") List<Long> ids,
+                                                                         @Param("deleted") Integer deleted);
+
+    /**
+     * 投影
+     */
+    interface ChannelPriceEntityProjection {
+        @Value("#{target.id}")
+        Long getId();
+
+        @Value("#{target.pmsSysCode}")
+        String getPmsSysCode();
+
+        @Value("#{target.access_platform}")
+        Integer getAccessPlatform();
+    }
 
 }
